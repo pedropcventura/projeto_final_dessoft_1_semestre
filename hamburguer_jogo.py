58fe38d69779e5d2632ee4498f5c21ab1c8034fb
eduardomontandon <emontandon3@gmail.com>
2022-05-23 20:46:26 -0300

    novos sprites e maciel se movimentando

--- hamburguer_jogo.py
+++ hamburguer_jogo.py
@@ -4,49 +4,51 @@ pygame.init()
 
 window = pygame.display.set_mode((600, 380))
 pygame.display.set_caption('hamburguers')
-fundo  = pygame.image.load('imagens/fundo restaurante.jpg').convert_alpha()
-cliente1 = pygame.image.load('imagens/cliente_homen.png').convert_alpha()
-cliente2 = pygame.image.load('imagens/cliente_mulher.png').convert_alpha()
-cliente3 = pygame.image.load('imagens/cliente_criança.png').convert_alpha()
-cliente3_menor = pygame.transform.scale(cliente3, (200, 300))
+fundo  = pygame.image.load('imagens/fundo.jpg').convert_alpha()
+fundo_menor = pygame.transform.scale(fundo, (600, 380))
 hamburguer = pygame.image.load('imagens/hamburguer.png').convert_alpha()
-hamburguer_menor = pygame.transform.scale(hamburguer, (80, 90))
-milkshake = pygame.image.load('imagens/milkshake.png').convert_alpha()
-milkshake_menor = pygame.transform.scale(milkshake, (80, 90))
+hamburguer_menor = pygame.transform.scale(hamburguer, (30, 40))
+cebola = pygame.image.load('imagens/cebola.png').convert_alpha()
+cebola_menor = pygame.transform.scale(cebola, (30, 40))
+maciel = pygame.image.load('imagens/maciel.png').convert_alpha()
+maciel_menor = pygame.transform.scale(maciel, (50, 60))
+   
 game = True
-tempo = 60
 vidas = 3
+class corredor (pygame.sprite.Sprite):
+    def __init__(self, imagem):
+        pygame.sprite.Sprite.__init__(self)
+        self.imagem = imagem 
+        self.rect = self.imagem.get_rect()
+        self.rect.x = 0
+        self.rect.y = 233
+        self.speedx = 3
 
-class cliente:
-    def __init__(self):
-        pass
-    def comer(self):
-        pass
-    def vai_embora(self):
-        pass
-
-class comida:
-    def __init__(self):
-        pass
-
-    def mover()
+    def update(self):
+        self.rect.x += self.speedx
+        if self.rect.x == 550:
+            self.speedx = 0
 
 
+cabeça_maciel = corredor(maciel_menor)
+
+clock = pygame.time.Clock()
+FPS = 30
 # ===== Loop principal =====
 while game:
+    clock.tick(FPS)
     # ----- Trata eventos
     for event in pygame.event.get():
         # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
-
     # ----- Gera saídas
+    cabeça_maciel.update()
     window.fill((255, 255, 255))  
-    window.blit(fundo, (0,0))
-    window.blit(cliente1,(100,100))
-    window.blit(cliente3_menor ,(300,100))
-    window.blit(hamburguer_menor,(250,100))
-    window.blit(milkshake_menor,(250,200))
+    window.blit(fundo_menor, (0,0))
+    window.blit(hamburguer_menor, (70, 260))
+    window.blit(cebola_menor, (120, 260))
+    window.blit(cabeça_maciel.imagem, cabeça_maciel.rect)
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
 
